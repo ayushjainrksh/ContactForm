28d6d39d34a12749e6c0d122735e1f3a474876ad
Ayush-Jain2 <ayushjainrksh@gmail.com>
2019-01-21 22:16:12 +0530

    Node server created using express

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+var express = require('express'),
+    app = express();
+
+app.get('/', function(req, res){
+    res.send("Hello...");
+});
+
+
+app.listen(4000, function(err){
+    if(err)
+      console.log(err);
+    else 
+      console.log("Server has started....");
+});
